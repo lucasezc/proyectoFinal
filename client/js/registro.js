e9c4ce618c82b8d9837501cc6eb927478adbd212
lucasezc <lucashubb@gmail.com>
2023-09-06 21:44:24 -0300

    .

--- client/js/registro.js
+++ client/js/registro.js
@@ -3,24 +3,22 @@ document.addEventListener("DOMContentLoaded", function () {
     const inputPw = document.getElementById("inputPw");
     const errorElement = document.getElementById("error");
     const emailSpan = document.getElementById("emailSpan");
-    const inputEmail = document.getElementById("email")
+    const inputEmail = document.getElementById("email");
 1
     // Recupera el correo electronico almacenado en localStorage
     const email = localStorage.getItem("email");
 
     // Muestra el correo electronico en la pagina de registro
-    if (email) {
-        emailSpan.textContent = email;
-    }
+        emailSpan.textContent = inputEmail.value;
+
 
     registroForm.addEventListener("submit", function (event) {
         event.preventDefault(); // Evitamos que se envie el formulario
 
-        // Obtenemos el valor del correo electronico
-
         // Redirige a la pagina de registro
         const createdPw = inputPw.value;
 
+        const email = emailInput.value;
         
         if (createdPw.length < 5) {
             errorElement.textContent = "La contraseña debe tener al menos 5 caracteres.";
